4d915c24f3c4ff7d6d4344367325ed179b800f4b
Joe Hartley <joejohnhartley@gmail.com>
2023-03-02 21:50:36 +0000

    adds new seed data to allow smarter filtering of artwork in collections, and adds the filter form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :artworks
+
+  include PgSearch::Model
+  multisearchable against: [:first_name, :last_name, :nationality, :location]
 end
