02f46e855e6fb50d4ce6544e699bea3a0e9df349
Lola Boatwright <lolaboatwright@gmail.com>
2023-03-04 17:22:50 +0000

    add david boatwright as user and one painting

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,21 @@ Collection.destroy_all
 ArtworkCollection.destroy_all
 
 puts "Creating user..."
-joey = User.create!(first_name: "Joey", last_name: "Artist", email: "joeyartist@email.com", password: "123456", nationality: "British", bio: "I'm a painter", date_of_birth: "1988-01-01", location: "London")
-jan = User.create!(first_name: "Jan", last_name: "Photographer", email: "janphotographer@email.com", password: "123456", nationality: "Slovakian", bio: "I'm a photographer", date_of_birth: "1990-01-01", location: "Berlin")
+
+david_boatwright = User.create!(first_name: "David", last_name: "Boatwright", email: "luckyboyart@comcast.net",
+  password: "123456", nationality: "American",
+  bio: "David Boatwright is a painter, known for his murals, as well as a musician and filmmaker.
+  From Camden, South Carolina, he holds an MFA in painting from the SF Art Institute and was a graduate fellow at AFI
+  filmschool in Los Angeles. In 1977, he received a grant from the National Endowment for the Arts to make documentary
+  films, resulting in the film Aimar’s, a pastiche of life in Charleston encapsulated by a pharmacy’s last day in
+  business. Boatwright always maintained a painting practice, which came to the forefront to his artistic career in the
+  1990s. Working under the mantle of Lucky Boy Art, Boatwright has painted more than 50 murals across South Carolina,
+  Georgia, and Louisiana. His studio oil on canvas work has also been exhibited at the Halsey Institute of Contemporary
+  Art, City Gallery at Waterfront Park, and Eye Level Art. In 2019, he was a visiting artist at the Gibbes Museum in
+  Charleston, SC, responding to a painting from their collection, “Plowing it Under” by Thomas Hart Benton, a distant
+  relative of Boatwright’s. His work often harkens to the South of his youth, employing mythical characters,
+  agricultural life, and alligators.", date_of_birth: "1947-05-26", degree: "MFA from SF Art Institute",
+  location: "Charleston")
 
 puts "Creating collections..."
 first_collection = Collection.new(title: "New & Noteworthy", description: "The crème de la crème of the newest Artichoke additions")
@@ -36,21 +49,12 @@ fourth_collection.photo.attach(io: file, filename: "fourth_collection.png", cont
 fourth_collection.save!
 
 puts "Creating art..."
-mona = { title: "Mona Lisa", price: 100, user: joey, medium: "painting", height: 77, width: 53, year: "1503-01-01", material: "Oil on poplar panel" }
-scream = { title: "The Scream", price: 200, user: joey, medium: "painting", height: 91, width: 73, year: "1893-01-01", material: "Oil, tempera, pastel and crayon on cardboard" }
-pitchfork = { title: "American Gothic", price: 300, user: jan, medium: "photography", height: 74, width: 62, year: "1930-01-01", material: "Oil on beaverboard" }
-diners = { title: "Nighthawks", price: 400, user: jan, medium: "photography", height: 84, width: 152, year: "1942-01-01", material: "Oil on canvas" }
 
-[mona, scream, pitchfork, diners].each do |attributes|
-  artwork = Artwork.new(attributes)
-  file = URI.open("https://source.unsplash.com/random/?#{attributes[:title]}")
-  artwork.photo.attach(io: file, filename: "#{attributes[:title]}.png", content_type: "image/png")
-  artwork.save!
-  puts "Created #{artwork.title}"
-  ArtworkCollection.create!(artwork: artwork, collection: first_collection)
-  ArtworkCollection.create!(artwork: artwork, collection: second_collection)
-  ArtworkCollection.create!(artwork: artwork, collection: third_collection)
-  ArtworkCollection.create!(artwork: artwork, collection: fourth_collection)
-end
+funk = Artwork.new(title: "Funk (Hardest Working Man)", price: 5000, user: david_boatwright, medium: "painting", height: 150, width: 300, year: "2020-01-01", material: "Oil on canvas")
+file = URI.open("http://www.luckyboyart.com/uploads/6/4/9/5/64954289/9932845_orig.jpg")
+funk.photo.attach(io: file, filename: "funk.jpg", content_type: "image/jpg")
+funk.save!
+
+ArtworkCollection.create!(artwork: funk, collection: first_collection)
 
 puts "Finished!"
