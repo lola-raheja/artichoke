0a43a23c933914616cb8b698949ce2ef8005f413
Lola Boatwright <lolaboatwright@gmail.com>
2023-03-11 17:19:43 +0000

    add slider MVP with next button functional on first click only

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    // console.log("Hello world!")
+    const artworks = document.querySelectorAll(".artwork-card-body");
+    // console.log(artworks)
+    artworks.forEach((artwork) => {
+      const position = parseInt(artwork.dataset.pos)
+      if (position >= 3 || position < 0) {
+        artwork.classList.add("hidden")
+      }
+    })
+  }
+
+  next() {
+    const artworks = document.querySelectorAll(".artwork-card-body");
+    artworks.forEach((artwork) => {
+      let position = parseInt(artwork.dataset.pos)
+      position -= 1
+      console.log(position)
+      if (position < 0) {
+        artwork.classList.add("hidden")
+      }
+      if (position > 0 && position < 3) {
+        artwork.classList.remove("hidden")
+      }
+      if (position > 3) {
+        artwork.classList.add("hidden")
+      }
+    })
+  }
+
+  previous() {
+    console.log("Previous button works")
   }
 }
