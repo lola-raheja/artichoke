2944cc8a876b986d4b075dbfcbba4cb02394c60b
Lola Boatwright <lolaboatwright@gmail.com>
2023-03-11 17:29:33 +0000

    update slider MVP with fully functional next and previous buttons

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,9 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    // console.log("Hello world!")
     const artworks = document.querySelectorAll(".artwork-card-body");
-    // console.log(artworks)
     artworks.forEach((artwork) => {
       const position = parseInt(artwork.dataset.pos)
       if (position >= 3 || position < 0) {
@@ -18,6 +16,7 @@ export default class extends Controller {
     artworks.forEach((artwork) => {
       let position = parseInt(artwork.dataset.pos)
       position -= 1
+      artwork.dataset.pos = position
       console.log(position)
       if (position < 0) {
         artwork.classList.add("hidden")
@@ -32,6 +31,21 @@ export default class extends Controller {
   }
 
   previous() {
-    console.log("Previous button works")
+    const artworks = document.querySelectorAll(".artwork-card-body");
+    artworks.forEach((artwork) => {
+      let position = parseInt(artwork.dataset.pos)
+      position += 1
+      artwork.dataset.pos = position
+      console.log(position)
+      if (position < 0) {
+        artwork.classList.add("hidden")
+      }
+      if (position > 0 && position < 3) {
+        artwork.classList.remove("hidden")
+      }
+      if (position > 3) {
+        artwork.classList.add("hidden")
+      }
+    })
   }
 }
