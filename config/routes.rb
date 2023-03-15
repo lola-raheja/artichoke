2fd167cab4980f778e6f730c6160ce0a45a2c0cf
Joe Hartley <joejohnhartley@gmail.com>
2023-03-15 13:00:30 +0000

    edits routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :collections, only: :show
   resources :bids, only: %i[show destroy edit update]
   resources :artworks, only: %i[index show] do
-    resources :bids, only: %i[create edit update]
+    resources :bids, only: %i[create update]
   end
   resources :users, only: :show do
     resources :bids, only: :index
