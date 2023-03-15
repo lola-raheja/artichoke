8c3a82c79d9057c43fc7b9749b5035ce91f0f729
Lola Boatwright <lolaboatwright@gmail.com>
2023-03-15 11:10:32 +0000

    add table associations between medium and artwork classes, moved medium validation to medium table

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,11 +1,9 @@
 class Artwork < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, :medium
   has_many :artwork_collections, dependent: :destroy
   has_many :collections, through: :artwork_collections
   has_many :bids, dependent: :destroy
   has_one_attached :photo
-
-  validates :medium, inclusion: { in: ["painting", "photography", "sculpture", "prints", "work on paper", "design", "drawing", "installation", "film/video"] }
 
   include PgSearch::Model
   pg_search_scope :global_search,
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,2 +1,5 @@
 class Medium < ApplicationRecord
+  has_many :artworks
+
+  validates :medium, inclusion: { in: ["painting", "photography", "multi-media", "sculpture", "print", "design", "drawing"] }
 end
