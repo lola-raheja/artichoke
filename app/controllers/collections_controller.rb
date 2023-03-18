d47e86dabac6ca51fe189a8684d749a11c62d2bf
Lola Boatwright <lolaboatwright@gmail.com>
2023-03-18 14:41:47 +0000

    ref assign method in show method, remove instance variable @ from artworks

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -9,28 +9,29 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = Collection.find(params[:id])
-    @artworks = @collection.artworks.order(title: :asc)
+    artworks = @collection.artworks.order(title: :asc)
     if params[:user_location].present?
-      @artworks = @artworks.joins(:user).where(users: { location: params[:user_location] })
+      artworks = artworks.joins(:user).where(users: { location: params[:user_location] })
     end
     if params[:medium_ids].present? && params[:medium_ids].size > 1
-      @artworks = @artworks.where(medium_id: params[:medium_ids])
+      artworks = artworks.where(medium_id: params[:medium_ids])
     end
     if params[:price].present?
       price_range = params[:price].split(',')
       price_min = price_range[0].to_i
       price_max = price_range[1].to_i
-      @artworks = @artworks.where(price: price_min..price_max)
+      artworks = artworks.where(price: price_min..price_max)
     end
     if params[:size].present?
       case params[:size]
       when 'small'
-        @artworks = @artworks.select { |artwork| artwork.average_size < 40 }
+        artworks = artworks.select { |artwork| artwork.average_size < 40 }
       when 'medium'
-        @artworks = @artworks.select { |artwork| artwork.average_size >= 40 && artwork.average_size <= 100 }
+        artworks = artworks.select { |artwork| artwork.average_size >= 40 && artwork.average_size <= 100 }
       when 'large'
-        @artworks = @artworks.select { |artwork| artwork.average_size > 100 }
+        artworks = artworks.select { |artwork| artwork.average_size > 100 }
       end
     end
+    assign_to_column(artworks)
   end
 end
